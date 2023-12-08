48e4e0851940321751a8487bd6d82897c84c420c
pedro bufulin <pedro.almeida@ufu.br>
2023-12-08 00:58:59 -0300

    day 07 not solved yet

--- src/bin/07.rs
+++ src/bin/07.rs
@@ -1,7 +1,38 @@
 advent_of_code::solution!(7);
 
 pub fn part_one(input: &str) -> Option<u32> {
+    // these are tuples, containing a duple of (card, bid), and a rank in the first value
+    let mut hands: Vec<(&str, u32)> = Vec::new();
+
+    for line in input.lines() {
+        let [cards, bid]: [&str; 2] = line
+            .split_whitespace()
+            .collect::<Vec<_>>()
+            .try_into()
+            .unwrap();
+        let bid_val = bid.parse::<u32>().unwrap();
+        hands.push((cards, bid_val))
+    }
+
+    //TODO: implemnt comparator.
+    hands.sort_by(|a, b| compare_cards(a.0, b.0));
+    println!("{:?}", hands);
     return Some(1);
+}
+
+// Define a function to compare two card lists
+fn compare_cards(new_cards: &str, existing_cards: &str) -> std::cmp::Ordering {
+    // Implement your logic to compare the card lists
+    use std::cmp::Ordering;
+
+    // Example comparison logic
+    if new_cards < existing_cards {
+        Ordering::Less
+    } else if new_cards > existing_cards {
+        Ordering::Greater
+    } else {
+        Ordering::Equal
+    }
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
