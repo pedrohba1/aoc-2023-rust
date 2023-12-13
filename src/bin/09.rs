73c538659911c0f575668414459d3d9a224994fd
pedro bufulin <pedro.almeida@ufu.br>
2023-12-12 23:17:19 -0300

    day 9, part 1 and two solutions

--- src/bin/09.rs
+++ src/bin/09.rs
@@ -4,7 +4,7 @@ use std::{collections::VecDeque, i32};
 use regex::Regex;
 
 pub fn parse(input: &str) -> Vec<Vec<i32>> {
-    let re = Regex::new(r"\w+").unwrap();
+    let re = Regex::new(r"\w+|-\w+").unwrap();
     let mut histories = Vec::new();
 
     for line in input.lines() {
@@ -21,44 +21,69 @@ pub fn parse(input: &str) -> Vec<Vec<i32>> {
 pub fn part_one(input: &str) -> Option<i32> {
     let histories = parse(input);
 
+    let mut acc = 0;
     for history in histories.iter() {
         let mut diffs: VecDeque<i32> = VecDeque::new();
         diffs.extend(history);
         let mut last_list = VecDeque::new();
-        last_list.push_front(diffs[diffs.len() - 1]);
         let mut aux = diffs;
         'outer: loop {
-            println!("aux list, {:?}", aux);
+            last_list.push_front(aux[aux.len() - 1]);
 
             let mut next_aux = VecDeque::new();
-            let mut diff_val = 0;
-            let last_val = aux[aux.len() - 1];
-            while aux.len() != 0 {
-                let (i1, i2) = (aux.pop_front().unwrap(), aux.pop_front().unwrap());
-                diff_val = i2 - i1;
-                next_aux.push_front(diff_val);
+
+            for i in 1..aux.len() {
+                let (i1, i2) = (aux[i - 1], aux[i]);
+                next_aux.push_back(i2 - i1);
             }
 
             // check if all values in the array are zero. If so, break the loop, use that
             if next_aux.iter().all(|&x| x == 0) {
-                last_list.push_front(diff_val);
                 break 'outer;
             }
-
-            last_list.push_front(last_val);
 
             // continue until the list gets to 0
             aux = next_aux;
         }
-        println!("last list, {:?}", last_list);
+
+        acc += last_list.iter().sum::<i32>();
     }
 
-    print!("{:?}", histories);
-    None
+    Some(acc)
 }
 
 pub fn part_two(input: &str) -> Option<i32> {
-    None
+    let histories = parse(input);
+
+    let mut acc = 0;
+    for history in histories.iter() {
+        let mut diffs: VecDeque<i32> = VecDeque::new();
+        let reversed: Vec<i32> = history.to_owned().into_iter().rev().collect();
+        diffs.extend(reversed);
+        let mut last_list = VecDeque::new();
+        let mut aux = diffs;
+        'outer: loop {
+            last_list.push_front(aux[aux.len() - 1]);
+            let mut next_aux = VecDeque::new();
+
+            for i in 1..aux.len() {
+                let (i1, i2) = (aux[i - 1], aux[i]);
+                next_aux.push_back(i2 - i1);
+            }
+
+            // check if all values in the array are zero. If so, break the loop, use that
+            if next_aux.iter().all(|&x| x == 0) {
+                break 'outer;
+            }
+
+            // continue until the list gets to 0
+            aux = next_aux;
+        }
+
+        acc += last_list.iter().sum::<i32>();
+    }
+
+    Some(acc)
 }
 
 #[cfg(test)]
@@ -68,12 +93,12 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(114));
     }
 
     #[test]
     fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        let result: Option<i32> = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(2));
     }
 }
